1f2aff29d0ae34416142a94e17bead3d1fef30a0
NadiaBejarano <nadiaveronicabejarano@gmail.com>
2023-10-06 19:42:54 -0300

    formularios

---
+++ src/pages/Presentation/sections/Comentario.js
@@ -0,0 +1,60 @@
+import { useState } from "react";
+
+// @mui material components
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
+import Switch from "@mui/material/Switch";
+
+// Material Kit 2 React components
+import MKBox from "components/MKBox";
+import MKInput from "components/MKInput";
+import MKButton from "components/MKButton";
+import MKTypography from "components/MKTypography";
+
+function Comentario() {
+  const [checked, setChecked] = useState(true);
+
+  const handleChecked = () => setChecked(!checked);
+
+  return (
+    <MKBox component="section" py={12}>
+      <Container>
+        <Grid container item justifyContent="center" xs={10} lg={7} mx="auto" textAlign="center">
+          <MKTypography variant="h3" mb={1}>
+            Tenes alguna experiencia para contarnos?
+          </MKTypography>
+        </Grid>
+        <Grid container item xs={12} lg={7} sx={{ mx: "auto" }}>
+          <MKBox width="100%" component="form" method="post" autocomplete="off">
+            <MKBox p={3}>
+              <Grid container spacing={3}>
+                <Grid item xs={12} md={6}>
+                  <MKInput variant="standard" label="Nombre" fullWidth />
+                </Grid>
+                <Grid item xs={12} md={6}>
+                  <MKInput variant="standard" label="Apellido" fullWidth />
+                </Grid>
+                <Grid item xs={12}>
+                  <MKInput variant="standard" type="email" label="Email Address" fullWidth />
+                </Grid>
+                <Grid item xs={12}>
+                  <MKInput variant="standard" label="Tu mensaje" multiline fullWidth rows={6} />
+                </Grid>
+                <Grid item xs={12} alignItems="center" ml={-1}>
+                  <Switch checked={checked} onChange={handleChecked} />
+                </Grid>
+              </Grid>
+              <Grid container item justifyContent="center" xs={12} my={2}>
+                <MKButton type="submit" variant="gradient" color="dark" fullWidth>
+                  Enviar Mensaje
+                </MKButton>
+              </Grid>
+            </MKBox>
+          </MKBox>
+        </Grid>
+      </Container>
+    </MKBox>
+  );
+}
+
+export default Comentario;
